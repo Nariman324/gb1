a8f7f85ce83c99be43552e91d1cede781d9f0f03
Nariman <tolkumbekov1996@mail.ru>
2022-12-08 19:37:19 +0600

    tasks commit

---
+++ z2/Program.cs
@@ -0,0 +1,28 @@
+﻿//Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел. (Сравнить с каждым)
+//2, 3, 7 -> 7
+//44 5 78 -> 78
+//22 3 9 -> 22
+
+Console.Write("Введите первое число: ");
+ int A = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+  int B = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите третье число: ");
+ int C = Convert.ToInt32(Console.ReadLine());
+
+
+if( A > B && A > C)
+{
+    Console.WriteLine( A + " Максимальное " );
+}
+    if( B > C && B > A)
+    {
+        Console.WriteLine( B + " Максимальное " );
+    }
+    if ( C > A && C > B)
+    {
+        Console.WriteLine( C + " Максимальное " );
+    }
+
